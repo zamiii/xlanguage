f6ea0dc5b461019cc139a8b26171df09e55b117c
zamiii <niguse180@gmail.com>
2025-04-24 12:29:55 +0200

    push

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 8000);
 
 app.get("/", (req, res) => {
   res.send("<h1> Hello, World! </h1>");
