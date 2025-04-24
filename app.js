d2a1e44b4698508c33e517e31a7e0053af4420aa
zamiii <niguse180@gmail.com>
2025-04-24 11:54:56 +0200

    pp

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
-app.listen(PORT, () => console.log("Running on port", PORT));
+app.listen(process.env.PORT || 3000);
 
 app.get("/", (req, res) => {
   res.send("<h1> Hello, World! </h1>");
